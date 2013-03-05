db85da8c7d3b6c814f78425ad254cf3cdec1882d
Nicholas Long <nicholas.long@nrel.gov>
2013-03-05 14:45:02 -0700

    adding template for profile.d. configuring for x64 centos

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'openstudio'
 maintainer       'NREL'
 maintainer_email 'nicholas.long@nrel.gov'
-license          'All rights reserved'
-description      'Installs/Configures openstudio'
+license          'LGPL'
+description      'Installs/Configures OpenStudio'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,42 @@
 # Cookbook Name:: openstudio
 # Recipe:: default
 #
-# Copyright 2013, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
+# Copyright 2013, NREL
+
+
+
+# handle the differing platforms
+remote_file "#{Chef::Config[:file_cache_path]}/OpenStudio-#{node[:openstudio][:version]}-Linux.sh" do
+  source "http://zerodev-128488.nrel.gov/openstudio/OpenStudio-#{node[:openstudio][:version]}-Linux.sh"
+  mode 00755
+  checksum node[:openstudio][:checksum]
+end
+
+
+# only works for 64 bit machines right now because it has to move lib directories:  x86_64-linux
+bash "install_openstudio" do
+  cwd Chef::Config[:file_cache_path]
+  code <<-EOH
+    chmod +x OpenStudio-#{node[:openstudio][:version]}-Linux.sh
+    OpenStudio-#{node[:openstudio][:version]}-Linux.sh --skip-license --include-subdir
+    mv OpenStudio-#{node[:openstudio][:version]}-Linux/usr/local /opt/OpenStudio-#{node[:openstudio][:version]}-Linux/
+
+  EOH
+
+  #mv /opt/OpenStudio-#{node[:openstudio][:version]}-Linux/
+  #rsync -a --delete-delay apache-solr-#{version}/dist/ #{node[:solr][:dist_dir]}
+  #rsync -a --delete-delay apache-solr-#{version}/contrib/ #{node[:solr][:contrib_dir]}
+
+  not_if { ::Dir.exists?("/opt/OpenStudio-#{node[:openstudio][:version]}-Linux/") }
+end
+
+template "/etc/profile.d/openstudio.sh" do
+  source "openstudio.sh.erb"
+  mode 00644
+end
+
+#bash "install_profile" do
+#  cwd "/etc/profile.d"
+  #export OPENSTUDIO_ROOT=/opt/OpenStudio-0.9.3.10197-Linux/
+  #export PATH=$OPENSTUDIO_ROOT/bin:$PATH
+#end
