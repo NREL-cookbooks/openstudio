997cbb61e5157b1a86b7e395b229155a17069b25
Nicholas Long <nicholas.long@nrel.gov>
2014-02-26 22:15:20 -0700

    bump openstudio versions

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,13 +3,13 @@ default[:openstudio][:install_method] = "installer"
 default[:openstudio][:add_library_to_path] = true
 
 # default versions
-default[:openstudio][:installer][:version] = "1.2.3"
-default[:openstudio][:installer][:version_revision] = "27c8fd5adf"
+default[:openstudio][:installer][:version] = "1.2.4"
+default[:openstudio][:installer][:version_revision] = "4bfc685068"
 default[:openstudio][:installer][:platform] = "Linux-Ruby2.0"
 default[:openstudio][:installer][:download_url] = "http://developer.nrel.gov/downloads/buildings/openstudio/builds"
 
 # for building openstudio on the node
-default[:openstudio][:source][:version] = "1.2.3"
+default[:openstudio][:source][:version] = "1.2.4"
 default[:openstudio][:source][:url] = "https://github.com/NREL/OpenStudio/archive"
 
 # by default it will build on n - 1 cores.  Uncomment if you want to explicitly define
